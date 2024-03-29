625f2281aa03b5038be7c9259f65580391c0e62f
MrMarvel <seregakkk999@yandex.ru>
2024-03-29 19:17:35 +0300

    added requirements for PIP and SETUPTOOLS in package

--- setup.py
+++ setup.py
@@ -22,5 +22,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    requires=[
+        'pip',
+        'setuptools',
     ]
 )
